7e7d146f35b6a473cfd0ad2f508e112225b876f6
steveejobs <bandeira.rgabriel@gmail.com>
2026-07-21 15:38:35 -0300

    Limpeza

--- components/test-vt2.tsx
+++
@@ -1,4 +0,0 @@
-import React, { ViewTransition } from 'react';
-export function Test() {
-  return <ViewTransition name="test"><div>Test</div></ViewTransition>;
-}
--- test-vt.tsx
+++
@@ -1,4 +0,0 @@
-import React, { ViewTransition } from 'react';
-export default function Test() {
-  return <ViewTransition name="test"><div>Test</div></ViewTransition>;
-}
